ef590d97eff74920815cdd25727a4f9c1647eacb
Randie B <randie.bemis@gmail.com>
2019-05-16 02:29:27 -0700

    dispatch login_user action or handle login failure

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import { GraphQLClient } from 'graphql-request';
 import { withStyles } from '@material-ui/core/styles';
 // import Typography from "@material-ui/core/Typography";
 
+import Context from '../../context';
+
 const graphqlEndpoint = 'http://localhost:4000/graphql';
 const graphqlQuery = `
 {
   me {
+    _id
     name
     email
     picture
@@ -16,19 +19,28 @@ const graphqlQuery = `
 `;
 
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context);
+
   const handleSuccess = async googleUser => {
-    const options = {
-      headers: { authorization: googleUser.getAuthResponse().id_token },
-    };
-    const graphqlClient = new GraphQLClient(graphqlEndpoint, options);
-    const data = await graphqlClient.request(graphqlQuery);
-    console.log('SUCCESS! data from graphql request:', data);
+    try {
+      const options = {
+        headers: { authorization: googleUser.getAuthResponse().id_token },
+      };
+      const graphqlClient = new GraphQLClient(graphqlEndpoint, options);
+      const { me } = await graphqlClient.request(graphqlQuery);
+      dispatch({ type: 'LOGIN_USER', payload: me });
+    } catch (error) {
+      handleFailure(error);
+    }
   };
+
+  const handleFailure = error => console.error('ERROR! Login failed', error);
 
   return (
     <GoogleLogin
       clientId="743566532889-sig9nstap9c5hil42h88cocv649i2ckt.apps.googleusercontent.com"
       onSuccess={handleSuccess}
+      onFailure={handleFailure}
       isSignedIn={true}
     />
   );
