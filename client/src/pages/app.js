bdb2b5a339d87f0466fca221901f1f05654dc44c
Randie B <randie.bemis@gmail.com>
2019-05-17 02:01:30 -0700

    display Map in app

--- client/src/pages/app.js
+++ client/src/pages/app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from '../components/header';
+import Map from '../components/map';
 import withRoot from '../with-root';
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Map />
+    </>
+  );
 };
 
 export default withRoot(App);
