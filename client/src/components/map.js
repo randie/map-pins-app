c6543f18d8a15596284da79640cb357659e23af5
Randie B <randie.bemis@gmail.com>
2019-05-17 02:44:52 -0700

    make map draggable and zoomable

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState } from 'react';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
@@ -8,22 +8,28 @@ import { withStyles } from '@material-ui/core/styles';
 const accessToken =
   'pk.eyJ1IjoicmFuZGllIiwiYSI6ImNqdnJ1M29nbDJ5NGw0YW11YTg5cmkyZ24ifQ.T-CIaru7GAEfY6iSTwdRGg';
 
-const viewport = {
+const initialViewport = {
   latitude: 45.512794,
   longitude: -122.679565,
   zoom: 13,
 };
 
 const Map = ({ classes }) => {
+  const [viewport, setViewport] = useState(initialViewport);
   return (
     <div className={classes.root}>
       <ReactMapGL
+        {...viewport}
         width="100vw"
         height="calc(100vh - 64px)"
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxApiAccessToken={accessToken}
-        {...viewport}
-      />
+        onViewportChange={newViewport => setViewport(newViewport)}
+      >
+        <div className={classes.navigationControl}>
+          <NavigationControl onViewportChange={newViewport => setViewport(newViewport)} />
+        </div>
+      </ReactMapGL>
     </div>
   );
 };
